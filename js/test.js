240af6818e4b2f148711c470cfb0cd41e460cee9
yuwq1098 <1098654043@qq.com>
2017-02-18 11:47:38 +0800

    backbone--麦子视频第三节课

--- js/test.js
+++ js/test.js
@@ -1,21 +1,86 @@
 $(function(){
     
-    var _obj = {
-    	title: "yuwenqiang",
-    	desription: "desription"
-    }
     
     var ToDoItem = Backbone.Model.extend({
 
     });
 
-    var toDoItem = new ToDoItem(_obj);
+    var toDoItem = new ToDoItem({
+        title: "yuwenqiang",
+    	desription: "desription"
+    });
+    
+    var ToDoItemView = Backbone.View.extend({
+        /*tagName: "li",
+        className: "cli",*/
+        /*attributes: {
+        	style: "color:red;height: 400px; background-color: #444;",
+        },*/
+        render: function(){
+            this.$el.html('<span>HEAD<span>');
+            return this;
+        },
+    })
 
-    console.log(toDoItem.get("title"));
+    var ToDoItemView02 = Backbone.View.extend({
+        tagName: "p",
+        className: "#talk",
 
-    toDoItem.set("title","yuwq123");
+        //重绘的方法
+        render: function(){
+            this.$el.html('<span>'+this.model.get("title")+'</span>&nbsp;&nbsp;is&nbsp;&nbsp;<span>'+this.model.get("desription")+'</span>');
+            return this;
+        },
+    })
 
-    console.log(toDoItem.toJSON().desription);
+
+
+    var toDoTtemView = new ToDoItemView({
+    	id: "item",
+    });
+    var toDoTtemView2 = new ToDoItemView({
+    	id: "item2",
+    });
+    var toDoTtemView3 = new ToDoItemView({
+    	/*tagName:"div",
+    	className: false,
+        id:"head",*/
+    	el: "#head",
+    });
+
+    toDoTtemView3.render();
+
+    var toDoTtemView4 = new ToDoItemView02({
+        model: toDoItem,
+    });
+    
+    toDoTtemView4.render().$el.appendTo($('body'));
+
+
+    console.log(toDoTtemView);
+    console.log(toDoTtemView.el.style);
+    
+    console.log(toDoTtemView2);
+
+    console.log(toDoTtemView3);
+    
+    console.log(toDoTtemView4);
+    
+    /*model更新后需要再次渲染*/
+    toDoItem.set("title","yuwq");
+    // toDoTtemView4.render().$el.appendTo($('body'));  不需要再次appendTo
+    toDoTtemView4.render();   
+
+    /*toDoTtemView4.remove();  //删除渲染
+    console.log(toDoTtemView4);
+
+    toDoTtemView4.render().$el.appendTo($('body'));
+    console.log(toDoTtemView4);
+*/
+
+
+
+
 
 });
 
