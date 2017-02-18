a5a716da239dede224d1d4d8a455298f8c59d30d
yuwq1098 <1098654043@qq.com>
2017-02-18 10:23:03 +0800

    第一课

--- js/test.js
+++ js/test.js
@@ -1,104 +1,21 @@
 $(function(){
     
-    //顶部导航条模型
-	var NavBarModel = Backbone.Model.extend(
-	{
-
-	});
-
-	//顶部导航条集合
-	var NavBarCollection = Backbone.Collection.extend(
-	{
-        model: NavBarModel,
-	});
-
-	/*实例化顶部导航条集合*/
-    var navBarCollection = new NavBarCollection(urlsMap);
+    var _obj = {
+    	title: "yuwenqiang",
+    	desription: "desription"
+    }
     
-    //顶部导航条视图
-	var NavBarView = Backbone.View.extend(
-	{
-        el: "ul.navbar",
-		events: {
-			"click li":  "cutStyle"
-		},
+    var ToDoItem = Backbone.Model.extend({
 
-		initialize: function() 
-		{
-			this.listenTo(this.model, "reset", this.render);
-		},
+    });
 
-		render: function() 
-		{
-			this.collection.each(function(model){
-				console.log(model);
-			})
-		},
+    var toDoItem = new ToDoItem(_obj);
 
-		cutStyle: function(e) 
-		{
+    console.log(toDoItem.get("title"));
 
-            var _this = this;
-			var index    = '';
-			//获取当前索引值
-			if(e) index  = $(e.currentTarget).index();
-			index = index ? index : 0;
-			this.index = index;
-            
-            /*获取当前点击的li元素*/
-			var _thisLi = _this.$el.find("li").eq(index);
+    toDoItem.set("title","yuwq123");
 
-			if(!_thisLi.hasClass("on"))
-			{
-                _thisLi.addClass("on").siblings("li").removeClass("on");
-			}
-		}
-
-	});
-    
-	/*实例化顶部导航条视图*/
-	var navBarView = new NavBarView({ collection : navBarCollection });
-
-	var leftNavView = Backbone.View.extend(
-	{
-        el: "section.navBar-left ul",
-		events: {
-			"click li":  "cutStyle"
-		},
-
-		initialize: function() 
-		{
-			this.listenTo(this.model, "change", this.render);
-		},
-
-		render: function() 
-		{
-			alert("aaa");
-		},
-
-		cutStyle: function(e) 
-		{
-
-            var _this = this;
-			var index    = '';
-			//获取当前索引值
-			if(e) index  = $(e.currentTarget).index();
-			index = index ? index : 0;
-			this.index = index;
-            
-            /*获取当前点击的li元素*/
-			var _thisLi = _this.$el.find("li").eq(index);
-
-			if(!_thisLi.hasClass("on"))
-			{
-                _thisLi.addClass("on").siblings("li").removeClass("on");
-			}
-		}
-
-	});
-    
-
-	var VleftNav = new leftNavView();
+    console.log(toDoItem.toJSON().desription);
 
 });
 
